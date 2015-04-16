04b9e3cae8c8ac862d0915651da6b481e54ba813
Lovinder Pnag <lspnag@gmail.com>
2015-04-15 22:51:38 -0700

    finished the bpopup functionality

--- TimeTracker/app/assets/javascripts/tasks.js
+++ TimeTracker/app/assets/javascripts/tasks.js
@@ -6,28 +6,28 @@ $(document).ready(function() {
 		var request = $.ajax({ url: $(this).attr("href"), type: "get"});
 			request.done(function(response){
 			$(".cr_teshow").append(response);
+			$("textarea").val("")
 		});
 	});
 
 	$(".cr_teshow").on("submit", "form", function (e) {
 		e.preventDefault();
-		// console.log("hello");
 
 		var link = $(this).attr("action");
 		var data = $(this).serialize();
+
+		$(".cr_teshow").bPopup().close()
 
 		var request = $.ajax({url: link, data: data, type: "post", dataType: "JSON"});
 			
 			request.done(function(data) {
 		var new_note = "<div class='note'>" +data.note+"</div>";
 		var new_btn = "<td><a id='edit_btn'href='/tasks/"+data.task_id+"/timeentries/"+data.id+"/edit'>Edit</a></td>"
-		
+
 		$(".note1").first().empty();
 		$(".note1").first().append(new_note);
 		$("tr").eq(1).append(new_btn);
-		// $(new_note).prependTo(".note")
 
-		console.log(new_note);
 
 			})
 	});
@@ -35,3 +35,15 @@ $(document).ready(function() {
 });
 
 
+$(".cr_teshow").ready(function(){
+	$("#start_btn").on("click", function (e) {
+		e.preventDefault();
+	$(".cr_teshow").bPopup({
+										appentTo: 'body'
+										, zIndex: 2
+										, opacity: 0.6
+										, modalClose: true
+										, transition: 'slideIn'
+		});
+	});
+});
